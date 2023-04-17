864785d022916ccef5cf5ee7771395535fe489f5
RobertGee <robertgouveia03@hotmail.com>
2023-04-17 05:59:25 +0100

    Breathing mechanic complete

--- Assets/Scripts/Actions/Breathing.cs
+++ Assets/Scripts/Actions/Breathing.cs
@@ -6,9 +6,16 @@ public class Breathing : MonoBehaviour
     [SerializeField] InputReaderSO inputReaderSO = default;
 
     [Header("UI")]
+    [SerializeField] GameObject UI;
     [SerializeField] GameObject mover;
-    [SerializeField] bool tempBreatheEvent = true;
+    [SerializeField] GameObject target;
+    [SerializeField] bool tempBreatheEvent = false;
     [SerializeField] bool holdinglocal = false;
+    [SerializeField] int targetPos;
+    [SerializeField] float leeway;
+
+    float lowbound = -0.8f;
+    float highbound = 0.8f;
 
     private void OnEnable()
     {
@@ -24,9 +31,44 @@ public class Breathing : MonoBehaviour
 
     private void Update()
     {
-        if (!tempBreatheEvent) return;
-        var dir = holdinglocal ? 0.001f : -0.001f;
-        mover.GetComponent<RectTransform>().position += new Vector3(0, dir, 0);
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            OnBreatheEvent();
+        }
+
+        if (tempBreatheEvent)
+        {
+            var dir = holdinglocal ? 0.001f : -0.001f;
+            if (mover.transform.localPosition.y < lowbound)
+                mover.transform.localPosition = new Vector3(mover.transform.localPosition.x, lowbound, mover.transform.localPosition.z);
+            if (mover.transform.localPosition.y > highbound)
+                mover.transform.localPosition = new Vector3(mover.transform.localPosition.x, highbound, mover.transform.localPosition.z);
+
+            if (!(mover.transform.localPosition.y > targetPos * 0.1f + leeway || mover.transform.localPosition.y < targetPos * 0.1f - leeway))
+            {
+                TargetHit();
+            }
+
+            mover.transform.localPosition += new Vector3(0, dir, 0);
+        }
+    }
+
+    private void TargetHit()
+    {
+        mover.transform.localPosition = Vector3.zero;
+        target.transform.localPosition = Vector3.zero;
+        UI.SetActive(false);
+        tempBreatheEvent = false;
+    }
+
+
+    //trigger on breathe event
+    private void OnBreatheEvent()
+    {
+        targetPos = Random.Range(8, -8);
+        UI.SetActive(true);
+        tempBreatheEvent = true;
+        target.transform.localPosition = new Vector3(target.transform.localPosition.x, targetPos * 0.1f, target.transform.localPosition.z);
     }
 
     private void OnBreathe(float input, bool holding)
